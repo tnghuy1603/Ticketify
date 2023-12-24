d9f51870e21f105f21480d3b7b747a58f04dba95
21120467 <21120467@student.hcmus.edu.vn>
2023-12-24 19:17:06 +0700

    update

--- front-end/src/components/ticketManagerPage/Content.jsx
+++ front-end/src/components/ticketManagerPage/Content.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import useAuth from "../../hooks/useAuth";
 
 
 function Content(params) {
+    const auth = useAuth()
+
+    const [movies, setMovies] = useState(null);
+
+
+    const getMovie = async () => {
+        try {
+            // Địa chỉ API và tham số
+            const apiUrl = 'http://localhost:8080/movies/manager';
+
+            // Gọi API bằng phương thức GET
+            const response = await fetch(apiUrl, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${auth.accessToken}`
+                }
+            });
+            // Kiểm tra trạng thái của response
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            else {
+                // Chuyển đổi response thành JSON
+                const result = await response.json();
+                // Cập nhật state với dữ liệu từ API
+                setMovies(result);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        getMovie();
+    }, []);
 
     return (
         <div>
