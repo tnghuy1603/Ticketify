1124cabf1c5667263c883f1bb4f3da250b8980ca
21120473 <anhdevwen@gmail.com>
2024-01-05 23:19:26 +0700

    Fix disable user and get disable user

--- back-end/src/main/java/project/intro2se/ticketify/controller/UserController.java
+++ back-end/src/main/java/project/intro2se/ticketify/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import project.intro2se.ticketify.domain.User;
 
 import project.intro2se.ticketify.dto.AddUserRequest;
+import project.intro2se.ticketify.dto.ToggleUserRequest;
 import project.intro2se.ticketify.service.UserService;
 
 import java.util.List;
@@ -16,16 +17,20 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
     @PutMapping
-    public ResponseEntity<User> disableUser(@RequestParam("user") Long userId){
-        return ResponseEntity.ok(userService.disableUser(userId));
+    public ResponseEntity<User> toggleUser(@RequestParam("user") Long userId, @RequestBody ToggleUserRequest request){
+        return ResponseEntity.ok(userService.toggleUser(userId, request.isLocked()));
     }
     @GetMapping
-    public ResponseEntity<List<User>> getAllEnabledUser(){
-        return ResponseEntity.ok(userService.getAllEnabledUser());
+    public ResponseEntity<List<User>> getUsers(@RequestParam(name = "locked", required = false) Boolean locked) {
+        if (locked == null) {
+            return ResponseEntity.ok(userService.findAll());
+        }
+        return ResponseEntity.ok(userService.findUserByLocked(locked));
     }
     @PostMapping
     public ResponseEntity<User> addUser(@RequestBody AddUserRequest request){
         return ResponseEntity.status(201).body(userService.addUser(request));
     }
+
 
 }
